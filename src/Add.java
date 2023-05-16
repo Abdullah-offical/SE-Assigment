fd0e54d13c0f0771c255d1d94c61bbb59d022569
khanmer <133587222+khanmer@users.noreply.github.com>
2023-05-15 21:51:55 -0700

    Add Sub Files

---
+++ src/Add.java
@@ -0,0 +1,6 @@
+// Division class
+class Add {
+    public static double div(double a, double b) {
+        return a + b;
+    }
+}
---
+++ src/Sub.java
@@ -0,0 +1,7 @@
+// Division class
+class Sub {
+    public static double div(double a, double b) {
+        
+        return a - b;
+    }
+}
